1a75a75b7be8fed21eee4d81d9bf1ca19967537c
Philipp Christmann <p.christmann@fh-bingen.de>
2019-09-21 19:22:11 +0200

    add tests with travisci

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestHandleLambdaEvent(t *testing.T) {
+
+    event := MyEvent {
+      Name: "Bob",
+      Age: 56,
+    }
+
+    response := MyResponse {Message: "Bob is 56 years old!"}
+
+		got, _ := HandleLambdaEvent(event)
+
+		if got != response {
+			t.Errorf("HandleLambdaEvent(%q) == %q, want %q", event, got, response)
+		}
+
+
+}
